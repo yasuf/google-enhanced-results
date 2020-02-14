86f33e6fb0595f349ea6827687decf8f064913a5
Yasu Flores <carlosyasu91@gmail.com>
2020-02-13 18:24:38 -0800

    add better selector to avoid selecting other elements within a result

--- content/google.js
+++ content/google.js
@@ -2,7 +2,7 @@ const alternativeResultsPresent =
   document.getElementsByClassName('TbwUpd NJjxre').length
 
 if (alternativeResultsPresent) {
-  const sElements = document.getElementsByClassName('s');
+  const sElements = document.querySelectorAll('.rc > .s');
   for(let i=0; i<sElements.length; i++) {
     sElements[i].setAttribute("style", "margin-top: 22px;");
   }
