1dca5ad2752e1f87608509c9c8b80c4d0844f7dc
Yasu Flores <carlosyasu91@gmail.com>
2020-02-13 22:45:45 -0800

    revert a few changes, since google results pages are different now

--- content/google.js
+++ content/google.js
@@ -1,9 +1,9 @@
-const alternativeResultsPresent =
-  document.getElementsByClassName('TbwUpd NJjxre').length
+// const alternativeResultsPresent =
+//   document.getElementsByClassName('TbwUpd NJjxre').length
 
-if (alternativeResultsPresent) {
-  const sElements = document.querySelectorAll('.rc > .s');
-  for(let i=0; i<sElements.length; i++) {
-    sElements[i].setAttribute("style", "margin-top: 22px;");
-  }
-}
+// if (alternativeResultsPresent) {
+//   const sElements = document.querySelectorAll('.rc > .s');
+//   for(let i=0; i<sElements.length; i++) {
+//     sElements[i].setAttribute("style", "margin-top: 22px;");
+//   }
+// }
